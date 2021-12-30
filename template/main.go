e80500bad3448ab330fccc27b5ed8eb7abb93a85
Léon Hollender <hello@leonh.fr>
2021-12-30 23:32:19 +0100

    feat: template

--- template/main.go
+++ template/main.go
@@ -17,14 +17,14 @@ func main() {
 	fmt.Printf("Part 2: %v\n", part2())
 }
 
-func init() {
-	lines = sh.Lines(input)
-}
-
 func part1() int {
 	return 0
 }
 
 func part2() int {
 	return 0
+}
+
+func init() {
+	lines = sh.Lines(input)
 }
